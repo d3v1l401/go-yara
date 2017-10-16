a86bd3a36e0b156b436b56ffd33bfe4132e29afc
d3v1l401 <lfrancioni@netsection.com>
2017-10-16 15:17:04 +0200

    fix?2

--- rules_yara34.go
+++ rules_yara34.go
@@ -44,7 +44,7 @@ func (r *Rules) ScanFileDescriptor(fd uintptr, flags ScanFlags, timeout time.Dur
 	defer callbackData.Delete(id)
 	err = newError(C._yr_rules_scan_fd(
 		r.cptr,
-		C.HANDLE(C.int(fd)),
+		C.HANDLE(fd),
 		C.int(flags),
 		C.YR_CALLBACK_FUNC(C.rules_callback),
 		unsafe.Pointer(id),
